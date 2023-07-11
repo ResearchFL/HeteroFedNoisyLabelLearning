a30fffb3233b5469f7eb3d563c93b7cc4c91f186
s2drag0n <2558058352@qq.com>
2023-07-11 21:07:49 +0800

    "update MR.sh again"

--- MR.sh
+++ MR.sh
@@ -4,9 +4,9 @@ level_n=1
 gpu_num=1
 begin_sel_r=5
 
-python main.py -alg FedCorr --dataset mnist --model lenet --rounds2 200 --lr 0.1 --num_users 100 --frac2 0.1 --gpu $gpu_num --level_n_system $level_n
-python main.py -alg FedCorr --dataset cifar10 --model resnet18 --rounds2 450 --lr 0.01 --num_users 100 --frac2 0.1 --gpu $gpu_num --level_n_system $level_n
-python main.py -alg FedCorr --dataset cifar100 --model resnet34 --rounds2 450 --lr 0.01 --num_users 50 --frac2 0.2 --gpu $gpu_num --level_n_system $level_n
-python main.py -alg FedCorr --dataset clothing1m --model resnet50 --rounds2 50 --lr 0.001 --num_users 300 --frac2 0.03 --gpu $gpu_num --level_n_system $level_n
+python main.py -alg MR --dataset mnist --model lenet --rounds2 200 --lr 0.1 --num_users 100 --frac2 0.1 --gpu $gpu_num --level_n_system $level_n
+python main.py -alg MR --dataset cifar10 --model resnet18 --rounds2 450 --lr 0.01 --num_users 100 --frac2 0.1 --gpu $gpu_num --level_n_system $level_n
+python main.py -alg MR --dataset cifar100 --model resnet34 --rounds2 450 --lr 0.01 --num_users 50 --frac2 0.2 --gpu $gpu_num --level_n_system $level_n
+python main.py -alg MR --dataset clothing1m --model resnet50 --rounds2 50 --lr 0.001 --num_users 300 --frac2 0.03 --gpu $gpu_num --level_n_system $level_n
 
 # python main.py -alg MR --dataset mnist --model resnet18 --rounds2 200 --lr 0.1 --plr 0.1 --num_users 100 --frac2 0.1 --begin_sel 5 --gpu 1 --level_n_system 1
